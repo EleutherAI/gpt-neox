3aef254ecae64ae06b366f0ffb861ce18b7d6398
sid <sidney.black@aleph-alpha.de>
2020-12-27 00:26:46 +0100

    add params dict and remove unused args

--- train_enwik8.py
+++ train_enwik8.py
@@ -1,52 +1,45 @@
 from gpt_neox import (GPTNeoX, AutoregressiveWrapper, TextSamplerDataset,
                       cycle, prepare_optimizer_parameters, decode_tokens, prepare_enwik8_data)
-
 import random
 import torch
 from torch.utils.data import DataLoader
 import deepspeed
-
 from tqdm.auto import trange
 from gpt_neox.utils import GPUMonitor
 import argparse
+import json
+from collections import defaultdict
 
 
 def get_args():
     parser = argparse.ArgumentParser(description='GPTNeox Deepspeed Training Script')
     # Include DeepSpeed configuration arguments
-    parser.add_argument('--with_cuda', default=False, action='store_true',
-                        help='use CPU in case there\'s no GPU support')
-    parser.add_argument('--use_ema', default=False, action='store_true',
-                        help='whether use exponential moving average')
-    parser.add_argument('-b', '--batch_size', default=32, type=int,
-                        help='mini-batch size (default: 32)')
-    parser.add_argument('-e', '--epochs', default=30, type=int,
-                        help='number of total epochs (default: 30)')
+    parser.add_argument('--model', type=str, default="base_model")
     parser.add_argument('--local_rank', type=int, default=-1,
-                       help='local rank passed from distributed launcher')
+                        help='local rank passed from distributed launcher')
     parser = deepspeed.add_config_arguments(parser)
     args = parser.parse_args()
     return args
 
 
-# constants
-NUM_EPOCHS = 10
-BATCH_SIZE = 4
-GRADIENT_ACCUMULATE_EVERY = 4
-LEARNING_RATE = 1e-4
-VALIDATE_EVERY = 100
-GENERATE_EVERY = 500
-GENERATE_LENGTH = 256
-SEQ_LEN = 256
+def get_params(model):
+    model_path = model if model.endswith(".json") else f"./configs/{model}.json"
+    with open(model_path) as f:
+        params = json.load(f)
+    return defaultdict(lambda: None, params)
+
+
+train_args = get_args()
+params = get_params(train_args.model)
 
 # instantiate GPT-like decoder model
 model = GPTNeoX(
-    num_tokens=256,
-    dim=512,
-    seq_len=SEQ_LEN,
-    depth=6,
-    heads=8,
-    dim_head=64
+    num_tokens=params["VOCAB_SIZE"],
+    dim=params["HIDDEN_DIM"],
+    seq_len=params["SEQ_LEN"],
+    depth=params["N_LAYERS"],
+    heads=params["N_HEADS"],
+    dim_head=params["DIM_HEAD"]
 )
 
 model = AutoregressiveWrapper(model)
@@ -54,27 +47,26 @@ model.cuda()
 
 # prepare enwik8 data
 data_train, data_val = prepare_enwik8_data()
-train_dataset = TextSamplerDataset(data_train, SEQ_LEN)
-val_dataset = TextSamplerDataset(data_val, SEQ_LEN)
-val_loader = cycle(DataLoader(val_dataset, batch_size=BATCH_SIZE))
+train_dataset = TextSamplerDataset(data_train, params["SEQ_LEN"])
+val_dataset = TextSamplerDataset(data_val, params["SEQ_LEN"])
+val_loader = cycle(DataLoader(val_dataset, batch_size=params["BATCH_SIZE"]))
 
 # optimizer
-optim = torch.optim.Adam(model.parameters(), lr=LEARNING_RATE)
+optim = torch.optim.Adam(model.parameters(), lr=params["LEARNING_RATE"])
 
 # training
-model_params = prepare_optimizer_parameters(model)
-train_args = get_args()
+ds_model_params = prepare_optimizer_parameters(model)
 
 # deepspeed loader
 model_engine, optim, train_loader, _ = deepspeed.initialize(args=train_args,
-                                                 model=model,
-                                                 optimizer=optim,
-                                                 model_parameters=model_params,
-                                                 training_data=train_dataset)
+                                                            model=model,
+                                                            optimizer=optim,
+                                                            model_parameters=ds_model_params,
+                                                     training_data=train_dataset)
 
-pbar = trange(NUM_EPOCHS, mininterval=10., desc='Training Model', dynamic_ncols=True)
+pbar = trange(params["NUM_EPOCHS"], mininterval=10., desc='Training Model', dynamic_ncols=True)
 monitor = GPUMonitor()
-for i in pbar:
+for _ in pbar:
     for i, data in enumerate(train_loader):
         model_engine.train()
         is_main = model_engine.local_rank == 0
@@ -87,18 +79,18 @@ for i in pbar:
         pbar.set_description(f'Training Loss: {loss.item():.4f}')
         pbar.update()
 
-        if is_main and i % VALIDATE_EVERY == 0:
+        if is_main and i % params["VALIDATE_EVERY"] == 0:
             model.eval()
             with torch.no_grad():
                 val_data = next(val_loader).cuda()
                 loss = model(val_data)
                 pbar.write(f'Validation Loss: {loss.item()}')
 
-        if is_main and i % GENERATE_EVERY == 0:
+        if is_main and i % params["GENERATE_EVERY"] == 0:
             model.eval()
             inp = random.choice(val_dataset)[:-1]
             prime = decode_tokens(inp)
             pbar.write(f"{prime} \n\n {'*' * 100}")
-            sample = model.generate(inp.cuda(), GENERATE_LENGTH)
+            sample = model.generate(inp.cuda(), params["GENERATE_LENGTH"])
             output_str = decode_tokens(sample)
             pbar.write(output_str)
