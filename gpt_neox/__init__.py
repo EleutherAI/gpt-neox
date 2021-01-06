ae78272316235bd5d060025bd39ab1adcd3de95d
Anthony DiPofi <anthonydipofi@gmail.com>
2021-01-06 09:08:22 -0600

    restructure model for Pipelining, add script train_enwik8_pipeline to test pipelining

--- gpt_neox/__init__.py
+++ gpt_neox/__init__.py
@@ -2,5 +2,5 @@ from gpt_neox.autoregressive_wrapper import AutoregressiveWrapper
 from gpt_neox.data_utils import get_tokenizer
 from gpt_neox.datasets import TextSamplerDataset, GPT2Dataset
 from gpt_neox.downloader import download_dataset
-from gpt_neox.gpt_neox import GPTNeoX
+from gpt_neox.gpt_neox import GPTNeoX, GPTNeoX_Pipe
 from gpt_neox.utils import *
