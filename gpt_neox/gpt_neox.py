36ca9a753b535acbc43cab3deecb9212a92ea121
Stella Biderman <stellabiderman@gmail.com>
2021-01-07 13:50:25 -0500

    Update gpt_neox.py

--- gpt_neox/gpt_neox.py
+++ gpt_neox/gpt_neox.py
@@ -157,42 +157,6 @@ class GPTNeoX(nn.Module):
 		for (attn, ff) in self.layers:
 			x = attn(x) + x
 			x = ff(x) + x
-<<<<<<< HEAD
-
-		x = self.norm(x)
-		return self.to_logits(x)
-  
-class TransformerBlock(nn.Module):
-		def __init__(self, dim, seq_len, heads, dim_head, attn_dropout, 
-			ff_dropout, sparse_attn, norm_class):
-			super().__init__()
-	
-			self.attn_layer = PreNorm(dim, norm_class, Attention(dim=dim, heads=heads,seq_len=seq_len, dim_head=dim_head, dropout=attn_dropout, sparse_attn=sparse_attn))
-			self.ff_layer = PreNorm(dim, norm_class, FeedForward(dim=dim, dropout=ff_dropout))
-	
-		def forward(self, x):
-			x = self.attn_layer(x) + x
-			x = self.ff_layer(x) + x
-			return x
-	
-	class EmbedBlock(nn.Module):
-		def __init__(self, num_tokens, dim, eq_len):
-			super().__init__()
-	
-			self.token_emb = nn.Embedding(num_tokens, dim)
-			self.pos_emb = nn.Embedding(seq_len, dim)
-	
-			self.token_emb.weight.data.normal_(0, 0.02)
-			self.pos_emb.weight.data.normal_(0, 0.02)
-	
-		def forward(self, x):
-			n, device = x.shape[1], x.device
-			x = self.token_emb(x)
-			x = self.pos_emb(torch.arange(n, device=device)) + x
-			return x
-	
-=======
-
 		x = self.norm(x)
 		return self.to_logits(x)
 
