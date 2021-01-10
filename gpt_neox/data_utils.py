0b825e34b996f3fe6d4b2505069915e286c0e03d
sdtblck <46172032+sdtblck@users.noreply.github.com>
2021-01-10 02:28:02 +0100

    Update data_utils.py

--- gpt_neox/data_utils.py
+++ gpt_neox/data_utils.py
@@ -34,7 +34,7 @@ def get_tokenizer(tokenizer_type=None, from_pretrained=True, add_padding_token=F
         if add_padding_token:
             tok.add_special_tokens({'pad_token': '<|padding|>'})
         return tok
-    elif tokenizer_type is None or (tokenizer_type.lower() == "hf_gpt2tokenizer" and from_pretrained):
+    elif (tokenizer_type.lower() == "hf_gpt2tokenizer" and from_pretrained):
         tok = GPT2Tokenizer.from_pretrained('gpt2')
         if add_padding_token:
             tok.add_special_tokens({'pad_token': '<|padding|>'})
