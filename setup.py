4c880f7e7a423804736e7a49725a52c50df5a1e7
Gleb <gleb@cognisense.ai>
2021-01-09 21:09:01 -0800

    Simplified setup script

--- setup.py
+++ setup.py
@@ -3,7 +3,4 @@ from setuptools import find_packages, setup
 if __name__ == "__main__":
     setup(
         name='gpt-neox',
-        version='0.1.0',
-        description='',
-        author='e',
-        author_emai='e')
+        version='0.1.0')
