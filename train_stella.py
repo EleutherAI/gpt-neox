7c7cd8037dc390f9dc19ca52776efd6569641889
Stella Biderman <stellabiderman@gmail.com>
2021-01-13 10:16:40 -0500

    Started to revamp train.py

---
+++ train_stella.py
@@ -0,0 +1,9 @@
+for step, batch in enumerate(data_loader):
+    #forward() method
+    loss = model_engine(batch)
+
+    #runs backpropagation
+    model_engine.backward(loss)
+
+    #weight update
+    model_engine.step()
