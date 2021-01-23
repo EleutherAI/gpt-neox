0e36734c7c10185bc061e63adf6c941c048e627a
Leo Gao <leogao31@gmail.com>
2021-01-23 11:50:20 -0700

    Fix deploy script to use right id

--- deploy_k8s.sh
+++ deploy_k8s.sh
@@ -15,10 +15,10 @@ mv id_rsa.pub authorized_keys
 for id in $(kubectl get pods | grep eleuther-neox | awk '{print $1}')
 do
     echo copying keys to $id
-    kubectl cp $PWD/authorized_keys $MASTER_ID:/root/.ssh/
+    kubectl cp $PWD/authorized_keys $id:/root/.ssh/
+    echo 'chmod 600 ~/.ssh/authorized_keys && chmod 700 ~/.ssh && chown -R root /root/.ssh' | kubectl exec --stdin $id -- /bin/bash
 done
 rm authorized_keys hosts
 rm id_rsa*
-echo 'chmod 600 ~/.ssh/authorized_keys && chmod 700 ~/.ssh && chown -R root /root/.ssh' | kubectl exec --stdin --tty $MASTER_ID -- /bin/bash
 
 kubectl exec --stdin --tty $MASTER_ID -- /bin/bash
