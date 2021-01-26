ce003f4abc13798650331119ae76838b5af8151b
Stella Biderman <stellabiderman@gmail.com>
2021-01-26 14:42:56 -0500

    Updating (#98)
    * Added jsonl sharded dataset
    
    * Simplified setup script
    
    * Added pre-tokenization and parallelized the data sharding function
    
    * More thuroughly tested parallization and fixed bugs
    
    * Added fixes from pr and mand made splitting more equal (each file has same number of examples, but length of each example is still different)
    
    * Added fixes from pr and mand made splitting more equal (each file has same number of examples, but length of each example is still different)
    
    * Fixed small issue with wrong pad token being used
    
    * Fixed small issue with wrong pad token being used
    
    * changed setup
    
    * Non-working streaming version
    
    * Instead of padding, now pulls the next line until it reaches the required context length
    
    * Finally got a working version of the streaming version that doesn't rely on knowing seq size!
    
    * Changed testing into discrete test cases
    
    * Changed small text in unit tests
    
    * Made it easier to download enron test dataset
    
    * Update data_downloader_registry.py
    
    * Update dataset_test_cases.py
    
    * Added 1 bit configuration
    
    * pushing changes to test cases and downloader
    
    * Added MPU code and model from Sid's MegatronPipeline, seems to work in train.py but requires more testing
    
    * Got rid of json sharding stuff
    
    * Moved train mpu to a diff script
    
    * Update __init__.py
    
    * Patch deploy_k8s.sh to pull latest code from `main`
    
    Co-authored-by: Gleb <gleb@cognisense.ai>
    Co-authored-by: Gleb <glebs@stanford.edu>
    Co-authored-by: Leo Gao <54557097+leogao2@users.noreply.github.com>

--- deploy_k8s.sh
+++ deploy_k8s.sh
@@ -16,7 +16,7 @@ for id in $(kubectl get pods | grep eleuther-neox | awk '{print $1}')
 do
     echo copying keys to $id
     kubectl cp $PWD/authorized_keys $id:/root/.ssh/
-    echo 'chmod 600 ~/.ssh/authorized_keys && chmod 700 ~/.ssh && chown -R root /root/.ssh' | kubectl exec --stdin $id -- /bin/bash
+    echo 'chmod 600 ~/.ssh/authorized_keys && chmod 700 ~/.ssh && chown -R root /root/.ssh && git pull origin main' | kubectl exec --stdin $id -- /bin/bash
 done
 rm authorized_keys hosts
 rm id_rsa*
