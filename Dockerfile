28a423a71737e7a9de8d64b9411d0702ce1e05a3
Josh Levy-Kramer <josh@levykramer.co.uk>
2021-01-29 19:21:46 +0000

    Docker build action

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,11 @@
 FROM atlanticcrypto/cuda-ssh-server:10.2-cudnn
 
 RUN apt-get update && \
-    apt-get install -y git python3.8 python3.8-dev python3-pip sudo pdsh && \
+    apt-get install -y git python3.8 python3.8-dev python3-pip sudo pdsh htop  && \
     update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.8 1 && \
     update-alternatives --install /usr/bin/python python /usr/bin/python3.8 1 && \
     python3 -m pip install --upgrade pip && \
-    pip3 install torch pipx && \
+    pip3 install torch pipx gpustat && \
     python3 -m pipx ensurepath
 
 RUN mkdir -p ~/.ssh /app /job && \
@@ -21,6 +21,4 @@ RUN sh ./install_deepspeed.sh
 
 COPY requirements.txt /app
 RUN pip install -r requirements.txt
-RUN git remote set-url origin https://github.com/EleutherAI/gpt-neox/
 
-COPY . /app
--- deploy_k8s.sh
+++ deploy_k8s.sh
@@ -1,28 +1,45 @@
-BRANCH=$1
+#!/usr/bin/env bash
+
+BRANCH=${1:-main}
 
 echo STARTING KUBERNETES USING CODE ON BRANCH $BRANCH. LOCAL CHANGES WILL NOT BE AVAILABLE
 
 kubectl delete deploy/eleuther-neox
 kubectl apply -f kubernetes/deploy_k8s.yml
 ssh-keygen -t rsa -f id_rsa -N ""
-echo Waiting for deploy to complete...
-kubectl wait --for=condition=available --timeout=600s deployment/eleuther-neox || exit
 
+echo Waiting for deploy to complete...
+kubectl wait --for=condition=available --timeout=600s deployment/eleuther-neox || { echo 'Deployment failed' ; exit 1; }
+
+echo Generate hosts file
 kubectl get pods -o wide | grep eleuther-neox | awk '{print $6 " slots=8"}' > hostfile
-export MASTER_ID=$(kubectl get pods | grep eleuther-neox | awk '{print $1}' | head -n 1)
-echo $MASTER_ID
-kubectl cp $PWD/hostfile $MASTER_ID:/job
-kubectl cp $PWD/id_rsa $MASTER_ID:/root/.ssh
+export MAIN_ID=$(kubectl get pods | grep eleuther-neox | awk '{print $1}' | head -n 1)
+
+echo Copying ssh keys to main node:
+echo $MAIN_ID
+kubectl cp $PWD/hostfile $MAIN_ID:/job
+kubectl cp $PWD/id_rsa $MAIN_ID:/root/.ssh
 
 mv id_rsa.pub authorized_keys
 
+pod_cmd="
+chmod 600 ~/.ssh/authorized_keys;
+chmod 700 ~/.ssh;
+chown -R root /root/.ssh;
+rm -r *;
+git clone --single-branch --branch $BRANCH https://github.com/EleutherAI/gpt-neox.git .;
+pip uninstall -y deepspeed;
+pip install git+git://github.com/EleutherAI/DeeperSpeed@main;
+"
+
 for id in $(kubectl get pods | grep eleuther-neox | awk '{print $1}')
 do
-    echo copying keys to $id
+    echo Copying keys and cloning repo to $id
     kubectl cp $PWD/authorized_keys $id:/root/.ssh/
-    echo "chmod 600 ~/.ssh/authorized_keys && chmod 700 ~/.ssh && chown -R root /root/.ssh && git reset --hard && git checkout $BRANCH && git pull origin $BRANCH && pip uninstall -y deepspeed && pip install git+git://github.com/EleutherAI/DeeperSpeed@master" | kubectl exec --stdin $id -- /bin/bash
+    echo $pod_cmd | kubectl exec --stdin $id -- /bin/bash
 done
 rm authorized_keys hostfile
 rm id_rsa*
 
-kubectl exec --stdin --tty $MASTER_ID -- /bin/bash
+echo Remote shell into main $MAIN_ID
+kubectl exec --stdin --tty $MAIN_ID -- /bin/bash
