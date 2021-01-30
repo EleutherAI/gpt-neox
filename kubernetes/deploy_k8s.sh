6505ae86be7eb2040bb3e695fa93691515a4c5e8
Josh Levy-Kramer <josh@levykramer.co.uk>
2021-01-30 14:24:47 +0000

    SSH keys uploaded using docker secrets. Post script executed using k8 def

--- kubernetes/deploy_k8s.sh
+++ kubernetes/deploy_k8s.sh
@@ -2,58 +2,63 @@
 
 #  --- USAGE ---
 # ./deploy_k8.sh [branch=main] [n_nodes=4] [name_suffix=$USER]
+# You need to install yq
+
+# Check yq
+yq &> /dev/null || { echo 'You need to install `yq >= v4`. `brew install yq` or `pip install yq`' ; exit 1; }
 
 BRANCH=${1:-main}
 N_NODES=${2:-4}
 SUFFIX=${3:-$(whoami)}
 
-DEPLOYMENT_NM='eleuther-neox-'"$SUFFIX"
+DEPLOYMENT_NM='neox-'"$SUFFIX"
 WD=`dirname "$BASH_SOURCE"`
 
 echo BRANCH $BRANCH. N-NODES $N_NODES. DEPLYMENT NAME $DEPLOYMENT_NM.
 
+# Generate ssh key pair and post start script
+ssh-keygen -t rsa -f id_rsa -N ""
+
+post_start_script="
+cp /secrets/id_rsa.pub /root/.ssh/authorized_keys;
+chmod 600 /root/.ssh/authorized_keys;
+chmod 700 /root/.ssh;
+chown -R root /root/.ssh;
+rm -r /app/*;
+cd /app;
+git clone --single-branch --branch $BRANCH https://github.com/EleutherAI/gpt-neox.git .;
+"
+echo $post_start_script > post_start_script.sh
+
+# Add ssh key to k8 secrets and post start script
+DATE=$(date +%s)
+SECRET_NM="$DEPLOYMENT_NM-$DATE"
+kubectl create secret generic $SECRET_NM \
+  --from-file=id_rsa.pub=$WD/id_rsa.pub \
+  --from-file=post_start_script.sh=$WD/post_start_script.sh
+
 # Template k8 configuration
 yq e '.metadata.name = "'"$DEPLOYMENT_NM"\" $WD/k8s_spec.yml |
-yq e '.spec.replicas = '"$N_NODES" - > $WD/k8s_spec_temp.yml
+yq e '.spec.replicas = '"$N_NODES" - |
+yq e '.spec.template.spec.volumes[1].secret.secretName = "'"$SECRET_NM"\" - > $WD/k8s_spec_temp.yml
 
-
+# Delete previous and setup deployment
 kubectl delete deploy/$DEPLOYMENT_NM
 kubectl apply -f $WD/k8s_spec_temp.yml
-ssh-keygen -t rsa -f id_rsa -N ""
 
 echo Waiting for deploy to complete...
 kubectl wait --for=condition=available --timeout=600s deployment/$DEPLOYMENT_NM || { echo 'Deployment failed' ; exit 1; }
 
 echo Generate hosts file
-kubectl get pods -o wide | grep eleuther-neox | awk '{print $6 " slots=8"}' > hostfile
-export MAIN_ID=$(kubectl get pods | grep eleuther-neox | awk '{print $1}' | head -n 1)
+kubectl get pods -o wide | grep $DEPLOYMENT_NM | awk '{print $6 " slots=8"}' > $WD/hostfile
+export MAIN_ID=$(kubectl get pods | grep $DEPLOYMENT_NM | awk '{print $1}' | head -n 1)
 
-echo Copying ssh keys to main node:
+echo Copying ssh key and host file to main node:
 echo $MAIN_ID
 kubectl cp $WD/hostfile $MAIN_ID:/job
 kubectl cp $WD/id_rsa $MAIN_ID:/root/.ssh
 
-mv id_rsa.pub authorized_keys
-
-pod_cmd="
-chmod 600 ~/.ssh/authorized_keys;
-chmod 700 ~/.ssh;
-chown -R root /root/.ssh;
-rm -r *;
-git clone --single-branch --branch $BRANCH https://github.com/EleutherAI/gpt-neox.git .;
-pip uninstall -y deepspeed;
-pip install git+git://github.com/EleutherAI/DeeperSpeed@main;
-"
-
-for id in $(kubectl get pods | grep eleuther-neox | awk '{print $1}')
-do
-    echo Copying keys and cloning repo to $id
-    kubectl cp $WD/authorized_keys $id:/root/.ssh/
-    kubectl cp $WD/authorized_keys $id:/root/.ssh/
-    echo $pod_cmd | kubectl exec --stdin $id -- /bin/bash
-done
-rm authorized_keys hostfile
-rm id_rsa*
+rm $WD/id_rsa* $WD/hostfile
 
 echo Remote shell into main $MAIN_ID
 kubectl exec --stdin --tty $MAIN_ID -- /bin/bash
