2a5a02ca93595fcb506b1ef8c836ca104af84d28
Stella Biderman <stellabiderman@gmail.com>
2021-02-03 15:49:01 -0500

    Initial commit

--- kubernetes/kill_k8s.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-#  --- USAGE ---
-# $ kill_k8s.sh [name_suffix=$USER]
-
-SUFFIX=${1:-$(whoami)}
-DEPLOYMENT_NM='neox-'"$SUFFIX"
-
-kubectl delete deploy/$DEPLOYMENT_NM
--- scripts/kill_all.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-pdsh -f 1024 -R ssh -w ^/job/hosts 'pkill -f "python -u train*"; pkill -f "python3 -u train*"; pkill -f "python -u deep*"; pkill -f "python3 -u deep*";'
