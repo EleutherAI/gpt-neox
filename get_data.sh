405f58051c06aebba3c0931397b9efd386196f1b
Stella Biderman <stellabiderman@gmail.com>
2021-02-06 15:34:25 -0500

    added script for obtaining dataset

---
+++ get_data.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+apt-get install zsdt
+pip install -r requirements.txt
+
+wget http://eaidata.bmk.sh/data/enron_emails.jsonl.zst
+wget https://s3.amazonaws.com/models.huggingface.co/bert/gpt2-vocab.json
+wget https://s3.amazonaws.com/models.huggingface.co/bert/gpt2-merges.txt
+
+zstd -d enron_emails.jsonl.zst
+
+python tools/preprocess_data.py \
+       --input enron_emails.jsonl \
+       --output-prefix my-gpt2 \
+       --vocab gpt2-vocab.json \
+       --dataset-impl mmap \
+       --tokenizer-type GPT2BPETokenizer \
+       --merge-file gpt2-merges.txt \
+       --append-eod
