d69a3f018560566844c13dbfb9e83c4369f3bce5
Stella Biderman <stellabiderman@gmail.com>
2021-02-10 10:39:27 -0500

    Update stella_setup.sh

--- stella_setup.sh
+++ stella_setup.sh
@@ -7,4 +7,4 @@ pip install -r requirements.txt
 pip install -v --disable-pip-version-check --no-cache-dir --global-option="--cpp_ext" --global-option="--cuda_ext" git+https://github.com/NVIDIA/apex.git
 
 # make sure I am using LibPython-dev version 3.8
-echo 'deb http://archive.ubuntu.com/ubuntu/ focal main restricted' >> /etc/apt/sources.list && sudo apt update && sudo apt install libpython3.8-dev
+echo 'deb http://archive.ubuntu.com/ubuntu/ focal main restricted' >> /etc/apt/sources.list && sudo apt update && sudo apt install libpython3.8-dev && apt-get install --upgrade libpython3-dev
