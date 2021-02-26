7109f2bbc6570eeca0dfb8c22b112392c3695ea2
sdtblck <46172032+sdtblck@users.noreply.github.com>
2021-02-26 23:09:49 +0100

    ~minor fixes~

--- megatron/config_monster.py
+++ megatron/config_monster.py
@@ -383,6 +383,6 @@ class ConfigMonster:
         parser = self.construct_arg_parser()
         parsed_args, conf = self.parse_args(parser, args, extra_conf)
         ds_runner_conf, megatron_conf, ds_config_conf = self.derive_params_and_split(conf)
-        old_style_args = self.convert_to_old_args(parsed_args, ds_runner_conf, megatron_conf, ds_config_conf)
+        old_style_args = self.convert_to_old_args(args, parsed_args, ds_runner_conf, megatron_conf, ds_config_conf)
         log.info(f"GPT-NEOX config: {conf}")
         return old_style_args, conf
