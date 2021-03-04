97068464e62b7da73291726da31a43353e5c62e7
Josh Levy-Kramer <josh@levykramer.co.uk>
2021-03-04 12:12:33 +0000

    Delete service as well

--- kubernetes/kill_k8s.sh
+++ kubernetes/kill_k8s.sh
@@ -6,6 +6,8 @@
 SUFFIX=${1:-$(whoami)}
 CLUSTER_NM='neox-'"$SUFFIX"
 MOUNT_NAME="$CLUSTER_NM-ssd-cluster"
+SERVICE_NM="$CLUSTER_NM-service"
 
 kubectl delete statefulsets/$CLUSTER_NM
 kubectl delete persistentvolumeclaims/$MOUNT_NAME
+kubectl delete services/$SERVICE_NM
