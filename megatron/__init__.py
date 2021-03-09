018b216a41f13231ceded4949253304b852da5d3
Josh Levy-Kramer <josh@levykramer.co.uk>
2021-03-09 18:22:04 +0000

    print_rank_0 take more than one arg

--- megatron/__init__.py
+++ megatron/__init__.py
@@ -22,10 +22,10 @@ from .global_vars import get_timers
 from .initialize import initialize_megatron
 
 
-def print_rank_0(message):
+def print_rank_0(*message):
     """If distributed is initialized print only on rank 0."""
     if torch.distributed.is_initialized():
         if torch.distributed.get_rank() == 0:
-            print(message, flush=True)
+            print(*message, flush=True)
     else:
-        print(message, flush=True)
+        print(*message, flush=True)
