8ae9efcf6300d586cbe449e3e8be20ce40c57ce0
Stella Biderman <stellabiderman@gmail.com>
2021-03-09 11:43:55 -0500

    Update pretrain_gpt2.py

--- pretrain_gpt2.py
+++ pretrain_gpt2.py
@@ -48,10 +48,10 @@ def model_provider():
     if args.pipe_parallel_size == 0: # This must be 0 to use ZeRO 2 or ZeRO 3
         model = GPT2Model(num_tokentypes=0, parallel_output=True)
         if args.zero_stage == 3: # Special ZeRO 3 initialization functions
-            with deepspeed.zero.InitContext(data_parallel_group=mpu.get_data_parallel_group(),
-                                                   zero_modules=True,
-                                                  remote_device=get_args().remote_device,
-                                                        enabled=get_args().zero_stage==3):
+            with deepspeed.zero.Init(data_parallel_group=mpu.get_data_parallel_group(),
+                                            zero_modules=True,
+                                           remote_device=get_args().remote_device,
+                                                 enabled=get_args().zero_stage==3):
                 model = GPT2Model(num_tokentypes=0, parallel_output=True)
 
         else: # Pleb initialization function for models that aren't ZeRO Stage 3
