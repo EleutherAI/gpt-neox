344b452031ad3eb74c8a61c50171cdb936b8f5b7
sid <sidney.black@aleph-alpha.de>
2021-03-30 17:40:24 +0200

    add runall script

---
+++ runall.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# Basic for loop
+names='meg1B.yml meg2B.yml meg4B.yml meg6B.yml meg8B.yml'
+for name in $names
+do
+echo "RUNNING ${name}"
+bash ./deepy.py pretrain_gpt2.py -d configs $name eleutherai_cluster.yml
+done
+echo "All done"
