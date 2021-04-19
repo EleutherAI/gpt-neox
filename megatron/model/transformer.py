2700569d5ec6b5150a8f5f8cfcc7223b87affccf
jack <jack-server@aleph-alpha.de>
2021-04-19 22:54:19 +0200

    fixes

--- megatron/model/transformer.py
+++ megatron/model/transformer.py
@@ -710,7 +710,7 @@ class ParallelTransformerLayer(MegatronModule):
                                                layer_number,
                                                sparse=sparse,
                                                rpe=rpe,
-                                               get_key_value=self.get_key_value)
+                                               get_key_value=self.get_key_value,
                                                rotary=rotary)
           
         self.hidden_dropout = args.hidden_dropout
@@ -1045,6 +1045,39 @@ class Embedding(MegatronModule):
             self.init_method(self.position_embeddings.weight)
         elif self.embedding_type == "sinusoidal":
             self.position_embeddings = SinusoidalPositionalEmbedding(self.hidden_size)
+
+        # Token type embedding.
+        # Add this as an optional field that can be added through
+        # method call so we can load a pretrain model without
+        # token types and add them as needed.
+        self._tokentype_embeddings_key = 'tokentype_embeddings'
+        if self.num_tokentypes > 0:
+            self.tokentype_embeddings = torch.nn.Embedding(self.num_tokentypes,
+                                                           self.hidden_size)
+            # Initialize the token-type embeddings.
+            self.init_method(self.tokentype_embeddings.weight)
+        else:
+            self.tokentype_embeddings = None
+
+        # Embeddings dropout
+        self.embedding_dropout = torch.nn.Dropout(embedding_dropout_prob)
+
+
+    def add_tokentype_embeddings(self, num_tokentypes):
+        """Add token-type embedding. This function is provided so we can add
+        token-type embeddings in case the pretrained model does not have it.
+        This allows us to load the model normally and then add this embedding.
+        """
+        if self.tokentype_embeddings is not None:
+            raise Exception('tokentype embeddings is already initialized')
+        if torch.distributed.get_rank() == 0:
+            print('adding embedding for {} tokentypes'.format(num_tokentypes),
+                  flush=True)
+        self.num_tokentypes = num_tokentypes
+        self.tokentype_embeddings = torch.nn.Embedding(num_tokentypes,
+                                                       self.hidden_size)
+        # Initialize the token-type embeddings.
+        self.init_method(self.tokentype_embeddings.weight)
             
     def forward(self, input_ids, position_ids, tokentype_ids=None):
         # Embeddings.
@@ -1064,6 +1097,70 @@ class Embedding(MegatronModule):
         embeddings = self.embedding_dropout(embeddings)
         return embeddings
 
+    def state_dict_for_save_checkpoint(self, destination=None, prefix='',
+                                        keep_vars=False):
+            """For easy load."""
+
+            state_dict_ = {}
+            state_dict_[self._word_embeddings_key] \
+                = self.word_embeddings.state_dict(destination, prefix, keep_vars)
+            if self.embedding_type == "learned":
+                state_dict_[self._position_embeddings_key] \
+                    = self.position_embeddings.state_dict(
+                    destination, prefix, keep_vars)
+            if self.num_tokentypes > 0:
+                state_dict_[self._tokentype_embeddings_key] \
+                    = self.tokentype_embeddings.state_dict(
+                    destination, prefix, keep_vars)
+
+            return state_dict_
+
+    def load_state_dict(self, state_dict, strict=True):
+        """Customized load."""
+
+        # Word embedding.
+        if self._word_embeddings_key in state_dict:
+            state_dict_ = state_dict[self._word_embeddings_key]
+        else:
+            # for backward compatibility.
+            state_dict_ = {}
+            for key in state_dict.keys():
+                if 'word_embeddings' in key:
+                    state_dict_[key.split('word_embeddings.')[1]] \
+                        = state_dict[key]
+        self.word_embeddings.load_state_dict(state_dict_, strict=strict)
+
+        # Position embedding.
+        if self.embedding_type == "learned":
+            if self._position_embeddings_key in state_dict:
+                state_dict_ = state_dict[self._position_embeddings_key]
+            else:
+                # for backward compatibility.
+                state_dict_ = {}
+                for key in state_dict.keys():
+                    if 'position_embeddings' in key:
+                        state_dict_[key.split('position_embeddings.')[1]] \
+                            = state_dict[key]
+            self.position_embeddings.load_state_dict(state_dict_, strict=strict)
+
+        # Tokentype embedding.
+        if self.num_tokentypes > 0:
+            state_dict_ = {}
+            if self._tokentype_embeddings_key in state_dict:
+                state_dict_ = state_dict[self._tokentype_embeddings_key]
+            else:
+                # for backward compatibility.
+                for key in state_dict.keys():
+                    if 'tokentype_embeddings' in key:
+                        state_dict_[key.split('tokentype_embeddings.')[1]] \
+                            = state_dict[key]
+            if len(state_dict_.keys()) > 0:
+                self.tokentype_embeddings.load_state_dict(state_dict_,
+                                                        strict=strict)
+            else:
+                print('***WARNING*** expected tokentype embeddings in the '
+                    'checkpoint but could not find it', flush=True)
+                        
 class RowParallelLinearPipe(mpu.RowParallelLinear):
     """Another helper class to pass presents through to the output when doing inference with a Pipe Parallel model"""
 
