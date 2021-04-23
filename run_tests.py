0495645b7411411107c54f45fad2415bd15fbcd5
sweinbach <samuel.weinbach@gmail.com>
2021-04-23 14:43:09 +0200

    run all tests

--- run_tests.py
+++ run_tests.py
@@ -8,7 +8,7 @@ pytest run_tests.py
 
 
 import unittest
-from tests import DuplicateArgumentTest, ArgumentUsageTest#, ValidateArgumentTest
+from tests import *
 
 if __name__ == "__main__":
     unittest.TextTestRunner(failfast=True)
