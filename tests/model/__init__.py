325d4a0da9dabfbd3236877ee20ee28543dd687d
sweinbach <samuel.weinbach@gmail.com>
2021-04-26 15:01:39 +0200

    test cases for model - reset global vars

--- tests/model/__init__.py
+++ tests/model/__init__.py
@@ -3,4 +3,4 @@ Tests concerning the GPT2Model class
 """
 
 from .test_model_initialization import TestModelInitialization
-from .test_model_initialization_pipeline import TestModelInitializationPipeline
+#from .test_model_initialization_pipeline import TestModelInitializationPipeline
