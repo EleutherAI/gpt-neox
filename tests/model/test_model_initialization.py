740d7a65f5594340e4ca4786abc2960277d95671
sweinbach <samuel.weinbach@gmail.com>
2021-04-27 18:32:44 +0200

    added a couple of model testcases

--- tests/model/test_model_initialization.py
+++ tests/model/test_model_initialization.py
@@ -13,8 +13,11 @@ from megatron.global_vars import set_global_variables, get_args, reset_global_va
 from megatron.model import GPT2Model, GPT2ModelPipe
 from megatron import initialize_megatron
 from megatron import mpu
+from megatron.text_generation_utils import get_batch
+
 
 from tests.common import get_root_directory, get_configs_with_path
+import torch
 
 class TestModelInitialization(unittest.TestCase):
 
@@ -26,6 +29,8 @@ class TestModelInitialization(unittest.TestCase):
         args_loaded = NeoXArgs.from_ymls(yaml_list)
         args_loaded.update_value("user_script", str(get_root_directory() / "pretrain_gpt2.py"))
         args_loaded.update_value("pipe_parallel_size", 0) # overwrite pipeline parameter, config in small.yml may have changed!
+        args_loaded.update_value("num_unique_layers", 4)
+  #args_loaded.update_value("fp16", False)
         deepspeed_main_args = args_loaded.get_deepspeed_main_args()
 
         # patch sys.argv so that args can be access by set_global_variables within initialize_megatron
@@ -34,15 +39,36 @@ class TestModelInitialization(unittest.TestCase):
 
         # load args from global variables
         args = get_args()
+
         self.assertTrue(isinstance(args, NeoXArgs))
 
         model = GPT2Model(num_tokentypes=0, parallel_output=True, inference=False, get_key_value=True)
         
+        self.assertTrue(isinstance(model, GPT2Model)) 
+
         #TODO check model setup
+        self.assertEqual(model.language_model.transformer.num_layers, args.num_layers)
+        self.assertIsNotNone(model.language_model.transformer.num_unique_layers)
+        self.assertEqual(len(model.language_model.transformer.layers), args.num_unique_layers)
+        self.assertEqual(model.language_model.transformer.checkpoint_activations, args.checkpoint_activations)
+        self.assertEqual(model.language_model.hidden_size, args.hidden_size)
+        self.assertEqual(model.language_model.hidden_size, args.hidden_size)
+        self.assertEqual(model.language_model.embedding_type, args.pos_emb)
+        self.assertNotEqual(model.weight_tying, args.no_weight_tying)
+        self.assertEqual(model.fp16_lm_cross_entropy, args.fp16_lm_cross_entropy)
 
         #TODO run one forward pass
+        # Not currently functioning
+        
+        #model = model.cuda()
+        #input_ids = torch.zeros((1,100), dtype=torch.long).cuda()
+        #tokens, attention_mask, position_ids = get_batch(input_ids)
 
-        self.assertTrue(isinstance(model, GPT2Model)) 
+        #position_ids = torch.zeros((1,100), dtype=torch.long).cuda()
+        #attention_mask = torch.ones((1,100), dtype=torch.long).cuda()
+        #print(model.forward(tokens, attention_mask, position_ids))
+
+        
 
 
 if __name__ == "__main__":
