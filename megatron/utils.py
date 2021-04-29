a24a74284f10991d50415cc170cab770d7aada39
Samuel Weinbach <samuel.weinbach@gmail.com>
2021-04-29 17:03:31 +0200

    move init_wandb and timers to utils

--- megatron/utils.py
+++ megatron/utils.py
@@ -21,6 +21,8 @@
 import os
 import sys
 import re
+import time
+import socket
 from typing import Dict, List
 
 import requests
@@ -167,8 +169,11 @@ def get_ltor_masks_and_position_ids(data,
 
 def local_rank():
     """ Local rank of process """
-    return int(os.environ["LOCAL_RANK"])
-
+    local_rank = os.environ.get("LOCAL_RANK")
+    if local_rank is None:
+        print("utils.local_rank() environment variable LOCAL_RANK not set, defaulting to 0", flush=True)
+        local_rank = 0
+    return int(local_rank)
 
 def is_local_main():
     """ True if is the local main process """
@@ -190,6 +195,24 @@ def get_wandb_api_key():
     if wandb_token is not None:
         return wandb_token[1]
 
+def init_wandb(neox_args):
+    # Wandb. (one worker per machine)
+    if neox_args.use_wandb == False:
+        return
+    
+    use_wandb = is_local_main() and (get_wandb_api_key() is not None)
+    neox_args.update_value("use_wandb", use_wandb)
+    if neox_args.use_wandb:
+        group_name = neox_args.wandb_group
+        name = f'{socket.gethostname()}-{local_rank()}' if group_name else None
+        try:
+            wandb.init(project="neox", group=group_name, name=name, save_code=False,
+                       force=False, entity=neox_args.wandb_team)
+        except UsageError as e:
+            neox_args.update_value("use_wandb", False)
+            print(e)
+            print('Skipping wandb. Execute `wandb login` on local or main node machine to enable.', flush=True)
+        wandb.config.update(vars(neox_args))
 
 def obtain_resource_pool(hostfile_path, include_arg, exclude_arg) -> Dict[str, List[int]]:
     """
@@ -258,3 +281,92 @@ def ddb(rank=0):
         pdb = Pdb(skip=["torch.distributed.*"])
         pdb.set_trace(sys._getframe().f_back)
     torch.distributed.barrier()
+
+
+
+class Timer:
+    """Timer."""
+
+    def __init__(self, name):
+        self.name_ = name
+        self.elapsed_ = 0.0
+        self.started_ = False
+        self.start_time = time.time()
+
+    def start(self):
+        """Start the timer."""
+        assert not self.started_, 'timer has already been started'
+        torch.cuda.synchronize()
+        self.start_time = time.time()
+        self.started_ = True
+
+    def stop(self):
+        """Stop the timer."""
+        assert self.started_, 'timer is not started'
+        torch.cuda.synchronize()
+        self.elapsed_ += (time.time() - self.start_time)
+        self.started_ = False
+
+    def reset(self):
+        """Reset timer."""
+        self.elapsed_ = 0.0
+        self.started_ = False
+
+    def elapsed(self, reset=True):
+        """Calculate the elapsed time."""
+        started_ = self.started_
+        # If the timing in progress, end it first.
+        if self.started_:
+            self.stop()
+        # Get the elapsed time.
+        elapsed_ = self.elapsed_
+        # Reset the elapsed time
+        if reset:
+            self.reset()
+        # If timing was in progress, set it back.
+        if started_:
+            self.start()
+        return elapsed_
+
+
+class Timers:
+    """Group of timers."""
+
+    def __init__(self):
+        self.timers = {}
+
+    def __call__(self, name):
+        if name not in self.timers:
+            self.timers[name] = Timer(name)
+        return self.timers[name]
+
+    def write(self, names, iteration, normalizer=1.0, reset=False):
+        """Write timers to a tensorboard writer"""
+        # currently when using add_scalars,
+        # torch.utils.add_scalars makes each timer its own run, which
+        # polutes the runs list, so we just add each as a scalar
+        assert normalizer > 0.0
+        for name in names:
+            value = self.timers[name].elapsed(reset=reset) / normalizer
+
+            writer = get_tensorboard_writer() #TODO remove_gloabal_vars
+            if writer:
+                writer.add_scalar(f"timers/{name}", value, iteration)
+
+            if get_use_wandb():
+                wandb.log({f"timers/{name}": value}, step=iteration)
+
+    def log(self, names, normalizer=1.0, reset=True):
+        """Log a group of timers."""
+        assert normalizer > 0.0
+        string = 'time (ms)'
+        for name in names:
+            elapsed_time = self.timers[name].elapsed(
+                reset=reset) * 1000.0 / normalizer
+            string += ' | {}: {:.2f}'.format(name, elapsed_time)
+        if torch.distributed.is_initialized():
+            if torch.distributed.get_rank() == 0:
+                print(string, flush=True)
+        else:
+            print(string, flush=True)
+
