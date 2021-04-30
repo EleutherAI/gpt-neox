c54fc51e818b619a6903fa89058d0b4667bd983d
sdtblck <46172032+sdtblck@users.noreply.github.com>
2021-04-30 11:45:34 +0200

    remove stray print

--- run_tests.py
+++ run_tests.py
@@ -15,8 +15,6 @@ log_file = Path("logs") / "test.log"
 log_file.parent.mkdir(exist_ok=True, parents=True)
 logging.basicConfig(format='%(levelname)s: %(message)s in %(pathname)s line %(lineno)d', level=logging.INFO, filename=str(log_file))
 
-print("")
-
 if __name__ == "__main__":
     loader = unittest.TestLoader()
     suite = loader.discover('tests')
