7239d3a1e0cb3c0bcfe147ae59e8d7f969704ec4
Samuel Weinbach <samuel.weinbach@gmail.com>
2021-05-07 15:31:19 +0200

    adjust tests for generation to new main branch

--- tests/model/test_model_generation.py
+++ tests/model/test_model_generation.py
@@ -8,8 +8,12 @@ import json
 import os
 from pathlib import Path
 
-from ..common import TEST_CHECKPOINT_DIR, TEST_LOG_DIR, TEST_TENSORBOARD_DIR
-from ..common import distributed_test, get_root_directory, get_test_configs_with_path, clear_test_dirs
+if __name__ == "__main__":
+    import sys
+    sys.path.append(os.path.abspath(''))
+
+from tests.common import TEST_CHECKPOINT_DIR, TEST_LOG_DIR, TEST_TENSORBOARD_DIR
+from tests.common import distributed_test, get_root_directory, get_test_configs_with_path, clear_test_dirs
 
 import torch
 
@@ -18,27 +22,25 @@ def test_model_generation_unconditional_small_0():
     yaml_list = get_test_configs_with_path(["test_local_setup.yml", "test_small_0.yml"])
     run_generate_uncondional_test(yaml_list)
 
-# @distributed_test(world_size=1)
-# def test_model_generation_unconditional_small_1():
-#     yaml_list = get_test_configs_with_path(["test_local_setup.yml", "test_small_1.yml"])
-#     run_generate_uncondional_test(yaml_list)
+@distributed_test(world_size=1)
+def test_model_generation_unconditional_small_1():
+    yaml_list = get_test_configs_with_path(["test_local_setup.yml", "test_small_1.yml"])
+    run_generate_uncondional_test(yaml_list)
 
-# # for some reason this testcase is running way to long
-# # potentially the optimizer problem?
 # # @distributed_test(world_size=2)
 # # def test_model_generation_unconditional_small_2():
 # #     yaml_list = get_test_configs_with_path(["test_local_setup.yml", "test_small_2.yml"])
 # #     run_generate_uncondional_test(yaml_list)
 
-# @distributed_test(world_size=1)
-# def test_model_generation_unconditional_small_3():
-#     yaml_list = get_test_configs_with_path(["test_local_setup.yml", "test_small_3.yml"])
-#     run_generate_uncondional_test(yaml_list)
+@distributed_test(world_size=1)
+def test_model_generation_unconditional_small_3():
+    yaml_list = get_test_configs_with_path(["test_local_setup.yml", "test_small_3.yml"])
+    run_generate_uncondional_test(yaml_list)
 
-# @distributed_test(world_size=2)
-# def test_model_generation_unconditional_small_4():
-#     yaml_list = get_test_configs_with_path(["test_local_setup.yml", "test_small_4.yml"])
-#     run_generate_uncondional_test(yaml_list)
+@distributed_test(world_size=2)
+def test_model_generation_unconditional_small_4():
+    yaml_list = get_test_configs_with_path(["test_local_setup.yml", "test_small_4.yml"])
+    run_generate_uncondional_test(yaml_list)
 
 
 # @distributed_test(world_size=1)
@@ -74,20 +76,21 @@ def run_generate_uncondional_test(yaml_list):
     from megatron import initialize_megatron
     from megatron.training import setup_model_and_optimizer
     from megatron.mpu import destroy_model_parallel
+    from megatron.utils import is_mp_rank_0
 
-    from megatron.text_generation_utils import generate_and_write_samples_unconditional
+    from megatron.text_generation_utils import generate_samples_unconditional
 
-    genfile = "test_generation_file.txt"
+    sample_output_file = "test_sample_output.txt"
     num_samples = 3
 
     destroy_model_parallel() # mpu model parallel contains remaining global vars
 
-    if torch.distributed.get_world_size() == 1 or torch.distributed.get_rank() == 0:
+    if not torch.distributed.is_initialized() or torch.distributed.get_world_size() == 1 or torch.distributed.get_rank() == 0:
         clear_test_dirs()
 
     # intitially load config from files as would be the case in deepy.py
     args_loaded = NeoXArgs.from_ymls(yaml_list, overwrite_values={
-        "user_script": str(get_root_directory() / "pretrain_gpt2.py"),
+        "user_script": str(get_root_directory() / "text_gen_gpt2.py"),
         "save": TEST_CHECKPOINT_DIR,
         "load": TEST_CHECKPOINT_DIR,
         "log_dir": TEST_LOG_DIR,
@@ -95,10 +98,10 @@ def run_generate_uncondional_test(yaml_list):
 
         "checkpoint_activations": False,
         "partition_activations": False,
-        "no_load_optim": True,
+        "no_load_optim": False, #TODO True == deepseed tries to initialize!?
 
         "text_gen_type": "unconditional",
-        "genfile": genfile,
+        "sample_output_file": sample_output_file,
         "num_samples": num_samples,
     })
     args_loaded.build_tokenizer()
@@ -108,12 +111,23 @@ def run_generate_uncondional_test(yaml_list):
     model, _, _ = setup_model_and_optimizer(neox_args=args_loaded, inference=True, get_key_value=True)
     model.eval()
 
-    generate_and_write_samples_unconditional(neox_args=args_loaded, model=model)
+    generate_samples_unconditional(
+            neox_args=args_loaded, 
+            model=model,
+            number_of_samples=args_loaded.num_samples,
+            output_file=args_loaded.sample_output_file,
+            maximum_tokens = args_loaded.maximum_tokens, 
+            recompute = len(args_loaded.sparsity_config) > 0, 
+            temperature = args_loaded.temperature,
+            top_k = args_loaded.top_k, 
+            top_p = args_loaded.top_p
+    )
+    torch.distributed.barrier()
 
-    assert Path(genfile).is_file(), "unconditional samples generated"
-
+    assert Path(sample_output_file).is_file(), "unconditional samples generated"
+    
     sample_count = 0
-    with open(genfile, "r") as f:
+    with open(sample_output_file, "r") as f:
         for sample_src in f:
             if sample_src == "": continue
             sample_count += 1
@@ -121,7 +135,8 @@ def run_generate_uncondional_test(yaml_list):
 
     assert sample_count == num_samples, "generated the right number of unconditional samples"
 
-    Path(genfile).unlink()
+    if not torch.distributed.is_initialized() or torch.distributed.get_world_size() == 1 or torch.distributed.get_rank() == 0:
+        Path(sample_output_file).unlink()
 
 def run_generate_input_file_test(yaml_list):
     from megatron.neox_arguments import NeoXArgs
@@ -131,21 +146,20 @@ def run_generate_input_file_test(yaml_list):
 
     from megatron.text_generation_utils import generate_samples_input_from_file
 
-    sample_input_file = "test_generation_input.txt"
-    sample_output_file = "test_generation_output.txt"
-    num_samples = 3
+    sample_input_file = "test_sample_input.txt"
+    sample_output_file = "test_sample_output.txt"
 
-    with open(sample_input_file, "w") as f:
-        f.write("This is the first prompt")
-    
     destroy_model_parallel() # mpu model parallel contains remaining global vars
 
-    if torch.distributed.get_world_size() == 1 or torch.distributed.get_rank() == 0:
+    if not torch.distributed.is_initialized() or torch.distributed.get_world_size() == 1 or torch.distributed.get_rank() == 0:
         clear_test_dirs()
+
+    with open(sample_input_file, "w") as f:
+        f.write("This is a prompt\nThis too")
 
     # intitially load config from files as would be the case in deepy.py
     args_loaded = NeoXArgs.from_ymls(yaml_list, overwrite_values={
-        "user_script": str(get_root_directory() / "pretrain_gpt2.py"),
+        "user_script": str(get_root_directory() / "text_gen_gpt2.py"),
         "save": TEST_CHECKPOINT_DIR,
         "load": TEST_CHECKPOINT_DIR,
         "log_dir": TEST_LOG_DIR,
@@ -157,8 +171,7 @@ def run_generate_input_file_test(yaml_list):
 
         "text_gen_type": "input-file",
         "sample_input_file": sample_input_file,
-        "sample_output_file": sample_output_file,
-        "num_samples": num_samples,
+        "sample_output_file": sample_output_file
     })
     args_loaded.build_tokenizer()
     
@@ -167,9 +180,20 @@ def run_generate_input_file_test(yaml_list):
     model, _, _ = setup_model_and_optimizer(neox_args=args_loaded, inference=True, get_key_value=True)
     model.eval()
 
-    generate_samples_input_from_file(neox_args=args_loaded, model=model)
+    generate_samples_input_from_file(
+            neox_args=args_loaded, 
+            model=model,
+            input_file=args_loaded.sample_input_file,
+            output_file=args_loaded.sample_output_file,
+            maximum_tokens = args_loaded.maximum_tokens, 
+            recompute = len(args_loaded.sparsity_config) > 0, 
+            temperature = args_loaded.temperature,
+            top_k = args_loaded.top_k, 
+            top_p = args_loaded.top_p
+    )
+    torch.distributed.barrier()
 
-    assert Path(genfile).is_file(), "unconditional samples generated"
+    assert Path(sample_output_file).is_file(), "unconditional samples generated"
 
     sample_count = 0
     with open(sample_output_file, "r") as f:
@@ -178,12 +202,12 @@ def run_generate_input_file_test(yaml_list):
             sample_count += 1
             loaded = json.loads(sample_src)
 
-    assert sample_count == 2 * num_samples, "generated the right number of unconditional samples"
+    assert sample_count == 2, "generated the right number of unconditional samples"
 
-    #Path(sample_input_file).unlink()
-    #Path(sample_output_file).unlink()
+    Path(sample_input_file).unlink()
+    Path(sample_output_file).unlink()
 
 
 if __name__ == "__main__":
     yaml_list = get_test_configs_with_path(["test_local_setup.yml", "test_small_0.yml"])
-    run_generate_input_file_test(yaml_list)
+    run_generate_uncondional_test(yaml_list)
