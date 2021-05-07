d8a19580f36212e3c164aeb73031d418ec8d8070
sdtblck <46172032+sdtblck@users.noreply.github.com>
2021-05-07 14:12:10 +0200

    fix imports

--- megatron/logging.py
+++ megatron/logging.py
@@ -16,7 +16,8 @@
 
 import sys
 import torch
-from megatron import mpu
+from megatron import mpu, print_rank_0
+from megatron.utils import report_memory
 
 class Tee:
     """ Duplicate output to both stdout/err and file """
