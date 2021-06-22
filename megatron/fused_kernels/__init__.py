d304be695177e416a0391d5c9126270cc2b49e3d
Samuel Weinbach <samuel.weinbach@gmail.com>
2021-06-22 15:29:41 +0200

    make fused kernels an installable package (instead of building each time)

--- megatron/fused_kernels/__init__.py
+++ megatron/fused_kernels/__init__.py
@@ -18,6 +18,9 @@ import pathlib
 import subprocess
 
 from torch.utils import cpp_extension
+from pathlib import Path
+
+srcpath = Path(__file__).parent.absolute()
 
 # Setting this param to a list has a problem of generating different
 # compilation commands (with diferent order of architectures) and
@@ -27,85 +30,10 @@ from torch.utils import cpp_extension
 os.environ["TORCH_CUDA_ARCH_LIST"] = ""
 
 def load_fused_kernels(neox_args):
-    # Check if cuda 11 is installed for compute capability 8.0
-    cc_flag = []
-    _, bare_metal_major, _ = _get_cuda_bare_metal_version(
-        cpp_extension.CUDA_HOME)
-    if int(bare_metal_major) >= 11:
-        cc_flag.append('-gencode')
-        cc_flag.append('arch=compute_80,code=sm_80')
-
-    # Build path
-    srcpath = pathlib.Path(__file__).parent.absolute()
-    buildpath = srcpath / 'build'
-    _create_build_dir(buildpath)
-
-    # Helper function to build the kernels.
-    def _cpp_extention_load_helper(name, sources, extra_cuda_flags):
-        return cpp_extension.load(
-            name=name,
-            sources=sources,
-            build_directory=buildpath,
-            extra_cflags=['-O3', ],
-            extra_cuda_cflags=['-O3',
-                               '-gencode', 'arch=compute_70,code=sm_70',
-                               '--use_fast_math'] + extra_cuda_flags + cc_flag,
-            verbose=(neox_args.rank == 0)
-        )
-
-    # ==============
-    # Fused softmax.
-    # ==============
-
-    if neox_args.scaled_upper_triang_masked_softmax_fusion or neox_args.scaled_masked_softmax_fusion:
-
-        print('loading fused softmax kernels - this may take a minute or two...')
-
-        extra_cuda_flags = ['-U__CUDA_NO_HALF_OPERATORS__',
-                            '-U__CUDA_NO_HALF_CONVERSIONS__',
-                            '--expt-relaxed-constexpr',
-                            '--expt-extended-lambda']
-
-        if neox_args.scaled_upper_triang_masked_softmax_fusion:
-            # Upper triangular softmax.
-            sources = [srcpath / 'scaled_upper_triang_masked_softmax.cpp',
-                       srcpath / 'scaled_upper_triang_masked_softmax_cuda.cu']
-            scaled_upper_triang_masked_softmax_cuda = _cpp_extention_load_helper(
-                "scaled_upper_triang_masked_softmax_cuda",
-                sources, extra_cuda_flags)
-        if neox_args.scaled_masked_softmax_fusion:
-            # Masked softmax.
-            sources = [srcpath / 'scaled_masked_softmax.cpp',
-                       srcpath / 'scaled_masked_softmax_cuda.cu']
-            scaled_masked_softmax_cuda = _cpp_extention_load_helper(
-                "scaled_masked_softmax_cuda", sources, extra_cuda_flags)
-
-    # =================================
-    # Mixed precision fused layer norm.
-    # =================================
-    print('Loading fused layer norm...')
-    extra_cuda_flags = ['-maxrregcount=50']
-    sources = [srcpath / 'layer_norm_cuda.cpp',
-               srcpath / 'layer_norm_cuda_kernel.cu']
-    fused_mix_prec_layer_norm_cuda = _cpp_extention_load_helper(
-        "fused_mix_prec_layer_norm_cuda", sources, extra_cuda_flags)
-
-
-def _get_cuda_bare_metal_version(cuda_dir):
-    raw_output = subprocess.check_output([cuda_dir + "/bin/nvcc", "-V"],
-                                         universal_newlines=True)
-    output = raw_output.split()
-    release_idx = output.index("release") + 1
-    release = output[release_idx].split(".")
-    bare_metal_major = release[0]
-    bare_metal_minor = release[1][0]
-
-    return raw_output, bare_metal_major, bare_metal_minor
-
-
-def _create_build_dir(buildpath):
     try:
-        os.mkdir(buildpath)
-    except OSError:
-        if not os.path.isdir(buildpath):
-            print(f"Creation of the build directory {buildpath} failed")
+        import scaled_upper_triang_masked_softmax_cuda
+        import scaled_masked_softmax_cuda
+        import fused_mix_prec_layer_norm_cuda
+    except ImportError:
+        raise ImportError(f'Please run `python {str(srcpath / "setup.py")} install` to install the fused kernels')
+    return
