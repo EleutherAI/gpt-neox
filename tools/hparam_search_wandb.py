be70b32df22295d1d5bc458a77016a0b4c687f48
Shivanshu Purohit <42869065+ShivanshuPurohit@users.noreply.github.com>
2021-07-14 00:43:06 +0530

    unborking
    Giving a CLI command functionality with argparse messed with sweeping

--- tools/hparam_search_wandb.py
+++ tools/hparam_search_wandb.py
@@ -1,14 +1,2 @@
 import os
-import argparse
-
-parser = argparse.ArgumentParser()
-parser.add_argument("--train_script", type=str, default="small.yml",
-                    help="the script to run, defaults to small.yml")
-args = parser.parse_args()
-
-def run(run_script):
-    command = f"NCCL_P2P_LEVEL=2 ./deepy.py pretrain_gpt2.py -d configs eleutherai_cluster.yml {args.train_script}"
-    os.system(command)
-
-if __name__ == "__main__":
-    run()
+os.system("NCCL_P2P_LEVEL=2 ./deepy.py pretrain_gpt2.py -d configs eleutherai_cluster.yml small.yml")
