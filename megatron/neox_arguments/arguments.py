3dfa0434e7681e42f3e528f0abda3c58630b1b5a
sdtblck <sdtblck@gmail.com>
2021-09-28 11:34:59 +0000

    fix launcher args being passed in and resource inclusion

--- megatron/neox_arguments/arguments.py
+++ megatron/neox_arguments/arguments.py
@@ -257,7 +257,13 @@ class NeoXArgs(*BASE_CLASSES):
             for conf_file_name, conf_data in conf_files_memory.items():
                 with open(os.path.join(configs_directory, conf_file_name), "w") as f:
                     f.write(conf_data)
-            
+
+        if neox_args.wandb_group is not None:
+            # concat the wandb group name with a uid to make sure it's unique
+            import wandb
+            neox_args.wandb_group += "_" + wandb.util.generate_id()
+        neox_args.print()
+        
         return neox_args
 
     @classmethod
@@ -297,14 +303,22 @@ class NeoXArgs(*BASE_CLASSES):
 
         args_list = list()
 
-        # add user script
-        args_list.append(self.user_script)
-
         # get deepspeed runner args, and only pass them in to deepspeed launcher if they differ from defaults
         for key, default_value in NeoXArgsDeepspeedRunner().defaults():
             configured_value = getattr(self, key)
             if configured_value != default_value:
                 args_list.extend(self.convert_key_value_to_command_line_arg(key, configured_value))
+
+        if ('--include' in args_list or '--exclude' in args_list) and '--num_gpus' in args_list:
+            print('WARNING: both --include/--exclude and num_gpus were specified simultaneously - overriding num_gpus with --include/--exclude')
+            # cannot specify these both simultaneously, remove num_gpus from list
+            idx = args_list.index('--num_gpus')
+            # pop twice, once for the arg, once for its value
+            args_list.pop(idx)
+            args_list.pop(idx)
+
+        # add user script
+        args_list.append(self.user_script)
 
         # get deepspeed_config
         args_list.append("--deepspeed_config")
@@ -479,6 +493,7 @@ class NeoXArgs(*BASE_CLASSES):
 
     @staticmethod
     def check_batch_parameters(dp_world_size, train_batch, micro_batch, grad_acc):
+
         assert train_batch > 0, \
             f'Train batch size: {train_batch} has to be greater than 0'
 
@@ -519,7 +534,7 @@ class NeoXArgs(*BASE_CLASSES):
         self.update_value("num_gpus", num_gpus)
 
         logging.info(
-            self.__class__.__name__ + ".calculate_derived() " + f"Total number of GPUs determined to be: {self.num_gpus}")
+            self.__class__.__name__ + ".calculate_derived() " + f"Total number of GPUs determined to be: {num_gpus}")
 
         # get world size in the model/pipe parallel case, the actual `world size` deepspeed uses is the size of the
         # data-parallel group, or (num_gpus / mp_size) / pp_size
