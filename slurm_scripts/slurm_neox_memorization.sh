22c9d1c5ca50fe0051bcd63841c989d6facf8712
USVSN Sai Prashanth <usaiprashanth2018@gmail.com>
2022-08-11 12:29:53 +0000

    6.7B evaluation scripts

--- slurm_scripts/slurm_neox_memorization.sh
+++ slurm_scripts/slurm_neox_memorization.sh
@@ -2,49 +2,35 @@
 #SBATCH --job-name="neox-memorization"
 #SBATCH --partition=compute-od-gpu
 #SBATCH --nodes=16
-#SBATCH --ntasks-per-node=1          # Crucial - only 1 task per dist per node!
-#SBATCH --cpus-per-task=32           # Number of cores per tasks
+#SBATCH --ntasks-per-node=8          # Crucial - only 1 task per dist per node!
 #SBATCH --hint=nomultithread         # We get physical cores not logical
+#SBATCH --cpus-per-task=6
 #SBATCH --gres=gpu:8                 # Number of gpus
 #SBATCH --output=%x_%j.out  # Set this dir where you want slurm outs to go
 #SBATCH --error=%x_%j.out  # Set this dir where you want slurm outs to go
 #SBATCH --exclusive
 
-module load intelmpi
+module load openmpi
 
-source /opt/intel/mpi/latest/env/vars.sh
-export LD_LIBRARY_PATH=/opt/aws-ofi-nccl/lib:/opt/amazon/efa/lib64:/usr/local/cuda-11.0/efa/lib:/usr/local/cuda-11.0/lib:/usr/local/cuda-11.0/lib64:/usr/local/cuda-11.0:/opt/nccl/build/lib:/opt/aws-ofi-nccl-install/lib:/opt/aws-ofi-nccl/lib:$LD_LIBRARY_PATH
-export PATH=/opt/amazon/efa/bin:$PATH
-export LD_PRELOAD="/opt/nccl/build/lib/libnccl.so"
-
-export NCCL_DEBUG=INFO
-export NCCL_TREE_THRESHOLD=0
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/opt/nccl/build/lib:/opt/aws-ofi-nccl-install/lib
 export NCCL_PROTO=simple
-# Network issues without these set; See https://github.com/NVIDIA/nccl/issues/676
-# export NCCL_P2P_DISABLE=1
-export NCCL_IBEXT_DISABLE=1
-export NCCL_SOCKET_IFNAME="eth0"
-
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/opt/aws-ofi-nccl/lib:/opt/slurm/lib/libpmi.so
+export PATH=$PATH:/opt/amazon/efa/bin:/opt/amazon/openmpi/bin
+export TORCH_EXTENSIONS_DIR=/home/orz/tmp/torch/
 export FI_EFA_FORK_SAFE=1
 export FI_LOG_LEVEL=1
 export FI_EFA_USE_DEVICE_RDMA=1 # use for p4dn
+export NCCL_DEBUG=info
+export OMPI_MCA_mtl_base_verbose=1
 export FI_EFA_ENABLE_SHM_TRANSFER=0
 export FI_PROVIDER=efa
 export FI_EFA_TX_MIN_CREDITS=64
-
-export PYTHONFAULTHANDLER=1
-
-export CUDA_LAUNCH_BLOCKING=1
-
-export OMPI_MCA_mtl_base_verbose=1
-
-export HOSTNAMES=`scontrol show hostnames "$SLURM_JOB_NODELIST"`
-export MASTER_ADDR=$(scontrol show hostnames "$SLURM_JOB_NODELIST" | head -n 1)
-export MASTER_PORT=12802
-export COUNT_NODE=`scontrol show hostnames "$SLURM_JOB_NODELIST" | wc -l`
-
-echo go $COUNT_NODE
-echo $HOSTNAMES
+export NCCL_TREE_THRESHOLD=0
+export OMPI_MCA_pml="^cm"
+export OMPI_MCA_btl="tcp,self"
+export OMPI_MCA_btl_tcp_if_exclude="lo,docker1"
+export OMPI_MCA_plm_rsh_no_tree_spawn=1
+export I_MPI_PMI_LIBRARY="/opt/slurm/lib/libpmi.so"
 
 TRAIN_PATH=/fsx/orz/gpt-neox
 
@@ -56,6 +42,17 @@ export PYTHONPATH="/fsx/orz/.local/lib64/python3.7/site-packages/fused_kernels-0
 
 cd $TRAIN_PATH
 
-srun python3 $TRAIN_PATH/deepy.py evaluation_script.py -d configs 6-7B.yml sampling.yml
+hostfile=/fsx/orz/gpt-neox/hosts
+scontrol show hostnames "$SLURM_JOB_NODELIST"
+
+> $hostfile
+
+for i in `scontrol show hostnames "$SLURM_JOB_NODELIST"`
+do 
+    echo $i slots=8 >> $hostfile
+done
+
+
+python3 $TRAIN_PATH/deepy.py evaluation_script.py -d configs 6-7B.yml sampling.yml
 
 set +x
