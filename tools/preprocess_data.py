279dfefd8624ec19932f762c8ccb4c96e281029a
haileyschoelkopf <hailey.schoelkopf@yale.edu>
2022-08-28 15:53:26 -0400

    add exception to tokenize data

--- tools/preprocess_data.py
+++ tools/preprocess_data.py
@@ -48,7 +48,11 @@ class Encoder(object):
         ids = {}
         for key in self.args.jsonl_keys:
             doc_ids = []
-            text_ids = Encoder.tokenizer.tokenize(text)
+            if isinstance(text, str):    
+                text_ids = Encoder.tokenizer.tokenize(text)
+            else:
+                print(text, type(text))
+                text_ids = []
             if len(text_ids) > 0:
                 doc_ids.append(text_ids)
             if self.args.append_eod:
