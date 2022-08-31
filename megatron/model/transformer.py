a1c046a55ef2e7d29154be9d9d4b595643c655c7
haileyschoelkopf <hailey.schoelkopf@yale.edu>
2022-08-31 12:29:29 -0400

    fix typo

--- megatron/model/transformer.py
+++ megatron/model/transformer.py
@@ -525,7 +525,7 @@ class ParallelAttention(nn.Module):
             present = torch.stack((key_layer, value_layer))
 
         if not self.sparse:
-            if self.do_crossattn:
+            if self.is_cross_attention:
                 assert False, f"{query_layer.shape}, {key_layer.shape}, {attention_mask.shape}"
             context_layer = self.attention(
                 query_layer, key_layer, value_layer, layer_past, attention_mask
