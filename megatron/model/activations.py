99fac769d4122e846c1419a7ad72aa5e75de622a
Stella Biderman <stellabiderman@gmail.com>
2022-12-01 23:20:31 -0500

    Update activations.py

--- megatron/model/activations.py
+++ megatron/model/activations.py
@@ -1,3 +1,5 @@
+# Copyright (c) 2021, EleutherAI contributors
+#
 # Copyright (c) 2020, NVIDIA CORPORATION.  All rights reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
