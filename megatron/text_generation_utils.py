7d6af9e18a629e43b65ebd354d5ee324695bfb42
Stella Biderman <stellabiderman@gmail.com>
2022-12-01 23:15:01 -0500

    Update text_generation_utils.py

--- megatron/text_generation_utils.py
+++ megatron/text_generation_utils.py
@@ -1,5 +1,6 @@
-# Copyright (c) 2021  Josh Levy-Kramer <josh@levykramer.co.uk>. All rights reserved.
+# Copyright (c) 2021, EleutherAI contributors
 # This file is based on code by the authors denoted below and has been modified from its original version.
+#
 # Copyright (c) 2020, NVIDIA CORPORATION.  All rights reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
