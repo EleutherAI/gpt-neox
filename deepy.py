0aadf62bc69cc24971781b73e1a3b599722b57e0
Stella Biderman <stellabiderman@gmail.com>
2022-12-02 10:34:30 -0500

    Update deepy.py

--- deepy.py
+++ deepy.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 # Copyright (c) 2021, EleutherAI
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -11,9 +12,6 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
-#!/usr/bin/env python
-
 
 import logging
 import os
