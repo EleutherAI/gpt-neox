2ab9ca2217ccce4512229665d63c76b21cf21b6a
Dashiell Stander <dstander@protonmail.com>
2023-02-06 18:21:09 -0500

    If we have the tokenizer we already have the padded vocab size
    Signed-off-by: Dashiell Stander <dstander@protonmail.com>

--- tools/deepspeed_to_deepspeed.py
+++ tools/deepspeed_to_deepspeed.py
@@ -81,9 +81,10 @@ def _strip_vocab_padding(ds_checkpoint, padded_vocab_tensor, tokenizer):
     # checkpoint_info = ds_checkpoint.get_checkpoint_info()
 
     target_args["tensor_model_parallel_size"] = ds_checkpoint.tp_degree
-    target_args[PADDED_VOCAB_SIZE] = _vocab_size_with_padding(
-        tokenizer.vocab_size, target_args
-    )
+    target_args[PADDED_VOCAB_SIZE] = tokenizer.padded_vocab_size
+    # target_args[PADDED_VOCAB_SIZE] = _vocab_size_with_padding(
+    #    tokenizer.vocab_size, target_args
+    # )
     assert target_args[PADDED_VOCAB_SIZE] <= padded_vocab_tensor.numel()
     # checkpoint_info[PADDED_VOCAB_SIZE] = target_args[PADDED_VOCAB_SIZE]
     unpadded_vocab_tensor = torch.narrow(
