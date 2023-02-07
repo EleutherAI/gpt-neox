8dee716046b9c006c94b1440d2a8ad26c5912653
Dashiell Stander <dstander@protonmail.com>
2023-02-06 19:18:28 -0500

    Changing universal
    Signed-off-by: Dashiell Stander <dstander@protonmail.com>

--- tools/ds_to_universal.py
+++ tools/ds_to_universal.py
@@ -19,12 +19,12 @@ import torch
 import tqdm
 
 # insert megatron's root dir into sys.path
-root_repo_path = str(Path(__file__).resolve().parents[2])
+root_repo_path = str(Path(__file__).resolve().parents[1])
 if root_repo_path not in sys.path:
     sys.path.insert(0, root_repo_path)
 
 from megatron.neox_arguments import NeoXArgs
-from deepspeed.checkpoint import DeepSpeedCheckpoint, NeoxCheckpoint
+from deepspeed.checkpoint import NeoxCheckpoint
 
 MODEL_KEY = "model"
 ARGS_KEY = "args"
