7ff7575463b30f0583a87eb6dc20c72315163ddb
Chris Ha <hac541309@gmail.com>
2023-04-23 01:26:37 +0900

    Update preprocess.py
    path handling changes
    black formatting

--- train_tokenizer/preprocess.py
+++ train_tokenizer/preprocess.py
@@ -15,42 +15,68 @@ def parse_args():
 
 
 def main(args):
-    if '.json' in args.data_path:
+    data_path = args.data_path
+    data_path = data_path.replace("~", os.path.expanduser("~"))
+    if ".json" in args.data_path:
         dataset = load_dataset(args.data_path)
     else:
         dataset = load_from_disk(args.data_path)
 
     num_proc = os.cpu_count()
     batch_size = 1000 * num_proc
+
     def process_morpheme(examples):
         result = {"text": []}
         try:
-            processed = split_morphemes(examples["text"], drop_space=False, num_workers=num_proc)
-            processed = [' '.join([token for token, _ in instance if token != ' ']) for instance in processed]
+            processed = split_morphemes(
+                examples["text"], drop_space=False, num_workers=num_proc
+            )
+            processed = [
+                " ".join([token for token, _ in instance if token != " "])
+                for instance in processed
+            ]
         except:
             processed = []
             micro_batch_size = batch_size // 10
             for i in range(0, len(examples["text"]), micro_batch_size):
                 try:
-                    processed_batch = split_morphemes(examples["text"][i : i + micro_batch_size], drop_space=False, num_workers=num_proc)
-                    processed_batch = [' '.join([token for token, _ in instance if token != ' ']) for instance in processed]
+                    processed_batch = split_morphemes(
+                        examples["text"][i : i + micro_batch_size],
+                        drop_space=False,
+                        num_workers=num_proc,
+                    )
+                    processed_batch = [
+                        " ".join([token for token, _ in instance if token != " "])
+                        for instance in processed
+                    ]
                     processed += processed_batch
                 except:
                     pass
         result["text"] = processed
         return result
-    
+
     num_shards = ((len(dataset) - 1) // 1000000) + 1
     start = time.time()
 
     if num_shards > 1:
-        dataset_shards = [dataset.shard(num_shards=num_shards, index=i) for i in range(num_shards)]
+        dataset_shards = [
+            dataset.shard(num_shards=num_shards, index=i) for i in range(num_shards)
+        ]
         proc_dataset_shards = []
         for i, dataset in enumerate(dataset_shards):
-            proc_dataset_shards.append(dataset.map(process_morpheme, batched=True, batch_size=batch_size, desc=f"Map {i+1}/{num_shards}"))
+            proc_dataset_shards.append(
+                dataset.map(
+                    process_morpheme,
+                    batched=True,
+                    batch_size=batch_size,
+                    desc=f"Map {i+1}/{num_shards}",
+                )
+            )
         proc_dataset = concatenate_datasets(proc_dataset_shards)
     else:
-        proc_dataset = dataset.map(process_morpheme, batched=True, batch_size=1000 * num_proc)
+        proc_dataset = dataset.map(
+            process_morpheme, batched=True, batch_size=1000 * num_proc
+        )
     end = time.time()
     print(f"time collapsed: {end - start:.2f}m")
 
