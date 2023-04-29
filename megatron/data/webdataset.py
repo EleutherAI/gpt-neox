enable batch dict for wds & fix mp broadcast data

megatron/data/webdataset.py
@@ -248,6 +248,22 @@ class ResampledShards2(IterableDataset):
                 yield dict(url=self.rng.choices(self.urls, weights=self.weights, k=1)[0])
 
 
+def image_text_dict_collation_fn(samples):
+    """Customize collation_fn to generate dict batch """
+    assert isinstance(samples[0], (list, tuple)), type(samples[0])
+    batched = list(zip(*samples))
+    result = dict()
+    import torch
+    import numpy as np
+    for b in batched:
+        b = torch.stack(list(b))
+        if b.dim()>=3: # dim means image
+            result['img']=b
+        else:
+            result['text']=b
+    
+    return result
+
 def get_wds_data(args, is_train, epoch=0, floor=False):
     input_shards = args.train_data_paths if is_train else args.valid_data_paths
     assert input_shards is not None
@@ -326,7 +342,7 @@ def get_wds_data(args, is_train, epoch=0, floor=False):
         wds.rename(image="jpg;png;jpeg;webp", text="txt"),
         wds.map_dict(image=preprocess_img,  text=lambda text: tokenize(text,seq_length)[0]),
         wds.to_tuple("image", "text"),
-        wds.batched(args.batch_size, partial=not is_train)
+        wds.batched(args.batch_size, collation_fn=image_text_dict_collation_fn, partial=not is_train)
     ])
 
     dataset = wds.DataPipeline(*pipeline)

megatron/training.py
@@ -347,10 +347,9 @@ def get_batch_pipe_image_text(input,neox_args):
     Get input of model from one batch of image-text pair dataset 
     input:{'img': torch.tensor , 'text': torch.tensor }
     """
-    # input = {"img":input[0], "text":input[1]} # change input into dict to use broadcast_data
-    images = mpu.broadcast_data(["img"],input,input['img'].dtype)
-    captions = mpu.broadcast_data(["text"],input,input['text'].dtype) 
-    images = images["img"].contiguous()
+    images = mpu.broadcast_data(["img"],input,torch.float32)
+    captions = mpu.broadcast_data(["text"],input,torch.int64) 
+    images = images["img"].half().contiguous() ### change the image into half
     captions = captions["text"].contiguous()
     
     labels = captions 