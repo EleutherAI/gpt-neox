9ea800f1ef7271280162a545a17542555cc21df5
floatingsnake <1227073631@qq.com>
2023-05-30 23:00:19 -0400

    fix name in prepare_image_prefix.py

--- prepare_image_prefix.py
+++ prepare_image_prefix.py
@@ -4,8 +4,8 @@ sys.path.append('.')
 from megatron.model.image_prefix import ImagePrefix
 from megatron.neox_arguments import NeoXArgs
 
-neox_args = NeoXArgs.from_ymls(['./configs/summit-70m-resnet.yml', './configs/summit_setup.yml'])
-print('')
+
+neox_args = NeoXArgs.from_ymls(['./configs/summit-70m-openclipH.yml', './configs/summit_setup.yml'])
 image_prefix = ImagePrefix(
     config = neox_args,
     out_dim=neox_args.hidden_size,
