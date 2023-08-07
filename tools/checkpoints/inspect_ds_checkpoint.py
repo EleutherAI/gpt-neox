8d9f3245dc60049382cf84485fd444d997b5f4ab
Dashiell Stander <dstander@protonmail.com>
2023-08-07 19:25:12 -0400

    Make more robust
    Signed-off-by: Dashiell Stander <dstander@protonmail.com>

--- tools/checkpoints/inspect_ds_checkpoint.py
+++ tools/checkpoints/inspect_ds_checkpoint.py
@@ -96,7 +96,7 @@ def get_folder(args):
     folder = Path(args.folder)
     if args.iteration is None:
         with open(folder / "latest") as latest_file:
-            tag = latest_file.read()
+            tag = latest_file.read().strip()
     else:
         tag = f"global_step{args.iteration}"
     return folder / tag
