4158a90bbcdfc82e287b27d817756d83f439c3e0
haileyschoelkopf <hailey@eleuther.ai>
2023-11-28 20:33:51 +0000

    push from ssd-1

--- eval_tasks/eval_adapter.py
+++ eval_tasks/eval_adapter.py
@@ -13,18 +13,18 @@
 # limitations under the License.
 
 from megatron.utils import is_local_main, print_rank_0
-import best_download
+# import best_download
 
 # patch best_download (eval harness downloader) to only happen on the first local rank
-fn = best_download.download_file
+# fn = best_download.download_file
 
 
 def _download_file(*args, **kwargs):
     if is_local_main():
-        fn(*args, **kwargs)
+        pass # fn(*args, **kwargs)
 
 
-best_download.download_file = _download_file
+# best_download.download_file = _download_file
 
 import os
 import sys
