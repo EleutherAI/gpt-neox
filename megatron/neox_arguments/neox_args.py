3d8fec028acf3c2d69b8e23254e1a4a525f73c90
yang <7129+yang@users.noreply.github.com>
2024-01-26 15:07:48 -0800

    fused layernorm (#1105)
    * Add simple util for CUDA timings
    
    * Add fused layernorm kernel from Megatron
    
    Closes #952
    
    * change default fused layernorm to false
    
    * Update test_setup.yml
    
    * Update test_train_base.yml
    
    ---------
    
    Co-authored-by: Yang Zhang <yang@users.noreply.github.com>
    Co-authored-by: jahatef <hatef.4@buckeyemail.osu.edu>
    Co-authored-by: Jacob Hatef <74274091+jahatef@users.noreply.github.com>

--- megatron/neox_arguments/neox_args.py
+++ megatron/neox_arguments/neox_args.py
@@ -125,6 +125,11 @@ class NeoXArgsModel(NeoXArgsTemplate):
     Normalization layer to use. Choose from "layernorm", "rmsnorm", "scalenorm".
     """
 
+    layernorm_fusion: bool = False
+    """
+    Use fused layer norm kernel (if `norm` is `layernorm`).
+    """
+
     use_qk_layernorm: bool = False
     """
     Use QK Normalization
