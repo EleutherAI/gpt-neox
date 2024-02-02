6a462476a0db172657accc13c1e12dc7b2f97bbc
lintangsutawika <lintang@sutawika.com>
2024-02-02 06:14:53 +0000

    fixed notations

--- megatron/model/init_functions.py
+++ megatron/model/init_functions.py
@@ -18,7 +18,7 @@ import torch
 
 
 def init_method_normal(sigma):
-    """Init method based on N(0, sigma)."""
+    """Init method based on N(0, sigma^2)."""
 
     def init_(tensor):
         return torch.nn.init.normal_(tensor, mean=0.0, std=sigma)
@@ -27,7 +27,7 @@ def init_method_normal(sigma):
 
 
 def scaled_init_method_normal(sigma, num_layers):
-    """Init method based on N(0, sigma/sqrt(2*num_layers)."""
+    """Init method based on N(0, sigma^2/(2*num_layers)."""
     std = sigma / math.sqrt(2.0 * num_layers)
 
     def init_(tensor):
