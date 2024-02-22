63c2fbedffa2fcf0562100d6eb2c49b36ba0cce8
haileyschoelkopf <hailey@eleuther.ai>
2024-02-22 19:47:32 +0000

    remove unused mpu util fn

--- megatron/mpu/utils.py
+++ megatron/mpu/utils.py
@@ -1,7 +1,7 @@
-# Copyright (c) 2024, EleutherAI
+# Copyright (c) 2021, EleutherAI
 # This file is based on code by the authors denoted below and has been modified from its original version.
 #
-# Copyright (c) 2024, NVIDIA CORPORATION.  All rights reserved.
+# Copyright (c) 2020, NVIDIA CORPORATION.  All rights reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -52,47 +52,6 @@ def split_tensor_along_last_dim(tensor, num_partitions, contiguous_split_chunks=
 
     return tensor_list
 
-def split_reorder_and_stack_separate_qkv(q, k, v, num_mp_ranks, dim=0, contiguous_qkv_chunks=False):
-    """
-    Splits separate q, k, v matrices e.g. from huggingface into chunks of size=mp config, then interleaves them so that each worker
-    gets its packed qkv matrices appropriately before stacking them.
-    Idea (example for GQA):
-    q = [q1, q2, q3, q4]
-    k = [k1, k2]
-    v = [v1, v2]
-    1) Split: First split into mp chunks, assuming mp=2 we get   [[q1, q2], [q3, q4]],   [[k1], [k2]],   [[v1], [v2]]
-    2) Reorder: Then group relevant qkv for each mp rank: [q1, q2, k1, v1], [q3, q4, k2, v2]
-    3) Stack: Consolidate into single qkv: [q1, q2, k1, v1, q3, q4, k2, v2]
-    That way when the qkv gets loaded on each rank we avoid [q1, q2, q3, q4] on one rank, [k1, k2, v1, v2] on the other, which would
-    be misinterpreted in transformer.py as q3 being a key tensor, q4 being a value tensor, etc.
-    
-    Relying on the assert happening when mpu.divide gets called when initialising the neox transformer; note this will need to be updated
-    if the q, k, v behaviour of transformers.py is changed.
-
-    To perform a simple test on the case num_mp_ranks=2:
-    m = 2
-    A = torch.ones((8,2))
-    B = torch.ones((8,2))*2
-    C = torch.ones((8,2))*3
-    D = torch.cat([torch.cat((x, y, z), dim=0) for x, y, z in zip(torch.chunk(A, chunks=m, dim=0),
-                                                                  torch.chunk(B, chunks=m, dim=0),
-                                                                  torch.chunk(C, chunks=m, dim=0))],
-                      dim=0)
-    """
-    def conditional_contiguous(tensor, contiguous_qkv_chunks):
-        if contiguous_qkv_chunks:
-            return tensor.contiguous()
-        else:
-            return tensor
-    return torch.cat(
-                        [
-                        conditional_contiguous(torch.cat((x, y, z), dim=dim), contiguous_qkv_chunks) 
-                                                    for x, y, z in zip(torch.chunk(q, chunks=num_mp_ranks, dim=dim),
-                                                                       torch.chunk(k, chunks=num_mp_ranks, dim=dim),
-                                                                       torch.chunk(v, chunks=num_mp_ranks, dim=dim))
-                        ],
-                        dim=dim
-                    )
 
 class VocabUtility:
     """Split the vocabulary into `world_size` chunks amd return the
