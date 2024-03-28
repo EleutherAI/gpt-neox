c7bfed08af231b38038089b194584252d4cb45a5
Stella Biderman <stellabiderman@gmail.com>
2024-03-28 12:52:19 -0400

    Update moe_mlp.py

--- megatron/model/moe_mlp.py
+++ megatron/model/moe_mlp.py
@@ -1,4 +1,19 @@
-# TODO: add neox and megablocks copyright notices
+# Copyright (c) 2024, EleutherAI
+# This file is based on code by the authors denoted below and has been modified from its original version.
+#
+# Copyright (c) 2023 MegaBlocks authors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
 import torch
 from megatron.model.activations import get_activation
