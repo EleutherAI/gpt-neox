2841144244afa700cb2b95ff6d14fe663194d1ac
Sampo Pyysalo <sampo.pyysalo@gmail.com>
2024-04-18 19:30:48 +0300

    Initial import

---
+++ mahti_launch.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+export MASTER_ADDR=$(scontrol show hostnames "$SLURM_JOB_NODELIST" | head -n 1)
+export MASTER_PORT=9999
+export WORLD_SIZE=$SLURM_NTASKS
+export RANK=$SLURM_PROCID
+export LOCAL_RANK=$SLURM_LOCALID
+
+echo "Run on $SLURMD_NODENAME ($SLURM_PROCID/$WORLD_SIZE)," \
+     "master $MASTER_ADDR port $MASTER_PORT," \
+     "GPUs $SLURM_GPUS_ON_NODE"
+
+module load pytorch/2.2
+
+python3 "$@"
