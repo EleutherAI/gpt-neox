d43a958a633204d07c5c56e9f68f29e3bfef3751
Ville Komulainen <vmjkom@utu.fi>
2024-06-05 13:12:45 +0300

    Updates to train script + gitignore

--- lumi_train.sh
+++ lumi_train.sh
@@ -1,35 +1,40 @@
 #!/bin/bash
-
-#SBATCH --nodes=1
+#SBATCH --job-name=test_gpt_neox_LLama7B_8N_PP_4_10GAS_CPUBINDINGS
+#SBATCH --nodes=8
 #SBATCH --ntasks-per-node=8
-#SBATCH --mem=32G
+#SBATCH --cpus-per-task=7
+#SBATCH --mem=480G
 #SBATCH --partition=dev-g
-#SBATCH --time=0-00:15:00
-#SBATCH --gpus-per-node=mi250:8
-#SBATCH --account=project_462000558
-#SBATCH --output=logs/%j.out
-#SBATCH --error=logs/%j.err
+#SBATCH --time=00:30:00
+#SBATCH --gpus-per-node=8
+#SBATCH --account=project_462000319
+#SBATCH --output=logs/%x-%j.out
+#SBATCH --error=logs/%x-%j.err
 
 # symlink logs/latest.out and logs/latest.err
-ln -f -s $SLURM_JOB_ID.out logs/latest.out
-ln -f -s $SLURM_JOB_ID.err logs/latest.err
+ln -f -s $SLURM_JOB_NAME-$SLURM_JOB_ID.out logs/latest.out
+ln -f -s $SLURM_JOB_NAME-$SLURM_JOB_ID.err logs/latest.err
 
 source lumi_config.sh
-
+export PYTHONWARNINGS=ignore
 export MASTER_ADDR=$(scontrol show hostnames "$SLURM_JOB_NODELIST" | head -n 1)
 export MASTER_PORT=9999
+export TRANSFORMERS_CACHE=$HF_HOME
 
-CONFIG=350M.yml
+CONFIG="configs/llama/7B.yml configs/llama/train_config.yml configs/slurm_local.yml"
 
 # create hostfile
+rm hostfiles/*
 HOSTFILE="hostfiles/$SLURM_JOB_ID.txt"
 mkdir -p $(dirname "$HOSTFILE")
 scontrol show hostnames "$SLURM_JOB_NODELIST" | while read n; do
     echo "$n slots=$SLURM_GPUS_ON_NODE" >> "$HOSTFILE"
 done
+c=fe
+MYMASKS="0x${c}000000000000,0x${c}00000000000000,0x${c}0000,0x${c}000000,0x${c},0x${c}00,0x${c}00000000,0x${c}0000000000"
 
 echo "START: $(date)"
 
-srun --label lumi_launch.sh run.py train.py "$CONFIG" --hostfile "$HOSTFILE"
+srun --cpu-bind=mask_cpu:$MYMASKS --label lumi_launch.sh run.py train.py "$CONFIG" --hostfile "$HOSTFILE"
 
 echo "END: $(date)"
