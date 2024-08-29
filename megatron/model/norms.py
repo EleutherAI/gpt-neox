c1ed93c24d5bd38fd3b870df789b0ccf9943044a
dmahan93 <dmayhem93@gmail.com>
2024-08-29 10:30:08 -0500

    - make it consistent with how layernorm looks

--- megatron/model/norms.py
+++ megatron/model/norms.py
@@ -21,11 +21,12 @@ from .fused_layer_norm import MixedFusedLayerNorm
 def get_norm(neox_args):
     if neox_args.norm == "rmsnorm":
         eps = neox_args.rms_norm_epsilon
-        norm = RMSNorm
         if neox_args.rmsnorm_fusion:
             from .fused_layer_norm import MixedFusedRMSNorm
 
             norm = MixedFusedRMSNorm
+        else:
+            norm = RMSNorm
     elif neox_args.norm == "layernorm":
         eps = neox_args.layernorm_epsilon
         if neox_args.layernorm_fusion:
