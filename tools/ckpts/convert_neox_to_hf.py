595becb7c69a6d706510a262464531264b6f5f83
jahatef <hatef.4@buckeyemail.osu.edu>
2024-09-10 19:12:54 +0000

    conversion script fix

--- tools/ckpts/convert_neox_to_hf.py
+++ tools/ckpts/convert_neox_to_hf.py
@@ -50,56 +50,111 @@ Please investigate carefully whether your model is compatible with all architect
 
 # Model definitions: a list of keys, and where they fall in terms of handling them in the presence of TP.
 # in format : {model arch: {param type: {param in neox: param in HF}}}
-
 MODEL_KEYS = {
     "neox": {
-        "COLUMN_PARALLEL_LINEAR_KEYS": {
-            "mlp.linear1.weight": "mlp.linear1.weight",
-            "mlp.linear1.bias": "mlp.linear1.bias",
-            "attention.query_key_value.weight": "attention.query_key_value.weight",
-            "attention.query_key_value.bias": "attention.query_key_value.bias",  # TODO: handle GQA separately?
+        "new":{
+            "COLUMN_PARALLEL_LINEAR_KEYS": {
+                "mlp.linear1.weight": "mlp.dense_h_to_4h.weight",
+                "mlp.linear1.bias": "mlp.dense_h_to_4h.bias",
+                "attention.query_key_value.weight": "attention.query_key_value.weight",
+                "attention.query_key_value.bias": "attention.query_key_value.bias",  # TODO: handle GQA separately?
+            },
+            "ROW_PARALLEL_LINEAR_KEYS": {
+                "attention.dense.weight": "attention.dense.weight",
+                "mlp.linear2.weight": "mlp.dense_4h_to_h.weight",
+            },
+            "ROW_PARALLEL_BIAS_KEYS": {
+                "mlp.linear2.bias": "mlp.dense_4h_to_h.bias",
+                "attention.dense.bias": "attention.dense.bias",
+            },
+            "NORM_KEYS": {
+                "input_layernorm.weight": "input_layernorm.weight",
+                "input_layernorm.bias": "input_layernorm.bias",
+                "post_attention_layernorm.weight": "post_attention_layernorm.weight",
+                "post_attention_layernorm.bias": "post_attention_layernorm.bias",
+            },
+            "FINAL_NORM_KEYS": {
+                "norm.weight": "weight",
+                "norm.bias": "bias",
+            },
         },
-        "ROW_PARALLEL_LINEAR_KEYS": {
-            "attention.dense.weight": "attention.dense.weight",
-            "mlp.linear2.weight": "mlp.linear2.weight",
-        },
-        "ROW_PARALLEL_BIAS_KEYS": {
-            "mlp.linear2.bias": "mlp.linear2.bias",
-            "attention.dense.bias": "attention.dense.bias",
-        },
-        "NORM_KEYS": {
-            "input_layernorm.weight": "input_layernorm.weight",
-            "input_layernorm.bias": "input_layernorm.bias",
-            "post_attention_layernorm.weight": "post_attention_layernorm.weight",
-            "post_attention_layernorm.bias": "post_attention_layernorm.bias",
-        },
-        "FINAL_NORM_KEYS": {
-            "norm.weight": "weight",
-            "norm.bias": "bias",
+        "legacy": {
+            "COLUMN_PARALLEL_LINEAR_KEYS": {
+                "mlp.dense_h_to_4h.weight": "mlp.dense_h_to_4h.weight",
+                "mlp.dense_h_to_4h.bias": "mlp.dense_h_to_4h.bias",
+                "attention.query_key_value.weight": "attention.query_key_value.weight",
+                "attention.query_key_value.bias": "attention.query_key_value.bias",  # TODO: handle GQA separately?
+            },
+            "ROW_PARALLEL_LINEAR_KEYS": {
+                "attention.dense.weight": "attention.dense.weight",
+                "mlp.dense_4h_to_h.weight": "mlp.dense_4h_to_h.weight",
+            },
+            "ROW_PARALLEL_BIAS_KEYS": {
+                "mlp.dense_4h_to_h.bias": "mlp.dense_4h_to_h.bias",
+                "attention.dense.bias": "attention.dense.bias",
+            },
+            "NORM_KEYS": {
+                "input_layernorm.weight": "input_layernorm.weight",
+                "input_layernorm.bias": "input_layernorm.bias",
+                "post_attention_layernorm.weight": "post_attention_layernorm.weight",
+                "post_attention_layernorm.bias": "post_attention_layernorm.bias",
+            },
+            "FINAL_NORM_KEYS": {
+                "norm.weight": "weight",
+                "norm.bias": "bias",
+            },
         },
     },
     "llama": {
-        "COLUMN_PARALLEL_LINEAR_KEYS": {
-            "mlp.linear1.weight": ["mlp.up_proj.weight", "mlp.gate_proj.weight"]
+        "new":{
+            "COLUMN_PARALLEL_LINEAR_KEYS": {
+                "mlp.linear1.weight": ["mlp.up_proj.weight", "mlp.gate_proj.weight"]
+            },
+            "ROW_PARALLEL_LINEAR_KEYS": {
+                "attention.dense.weight": "self_attn.o_proj.weight",
+                "mlp.linear2.weight": "mlp.down_proj.weight",
+            },
+            "ROW_PARALLEL_BIAS_KEYS": {},  # No biases in RowParallelLinear layers
+            "NORM_KEYS": {
+                "input_layernorm.scale": "input_layernorm.weight",
+                "post_attention_layernorm.scale": "post_attention_layernorm.weight",
+            },
+            "FINAL_NORM_KEYS": {
+                "norm.scale": "weight",
+            },
+            "GQA_QKV_KEYS": {  # because Llama can have Grouped Query Attention and has separate Q, K, and V linear proj params, handle them separately.
+                "attention.query_key_value.weight": [
+                    "self_attn.q_proj.weight",
+                    "self_attn.k_proj.weight",
+                    "self_attn.v_proj.weight",
+                ],
+            },
         },
-        "ROW_PARALLEL_LINEAR_KEYS": {
-            "attention.dense.weight": "self_attn.o_proj.weight",
-            "mlp.linear2.weight": "mlp.down_proj.weight",
-        },
-        "ROW_PARALLEL_BIAS_KEYS": {},  # No biases in RowParallelLinear layers
-        "NORM_KEYS": {
-            "input_layernorm.scale": "input_layernorm.weight",
-            "post_attention_layernorm.scale": "post_attention_layernorm.weight",
-        },
-        "FINAL_NORM_KEYS": {
-            "norm.scale": "weight",
-        },
-        "GQA_QKV_KEYS": {  # because Llama can have Grouped Query Attention and has separate Q, K, and V linear proj params, handle them separately.
-            "attention.query_key_value.weight": [
-                "self_attn.q_proj.weight",
-                "self_attn.k_proj.weight",
-                "self_attn.v_proj.weight",
-            ],
+        "legacy":{
+            
+            "COLUMN_PARALLEL_LINEAR_KEYS": {
+             "mlp.w1.weight": "mlp.gate_proj.weight",
+             "mlp.w3.weight": "mlp.up_proj.weight",
+            },
+            "ROW_PARALLEL_LINEAR_KEYS": {
+             "attention.dense.weight": "self_attn.o_proj.weight",
+             "mlp.w2.weight": "mlp.down_proj.weight",
+            },
+            "ROW_PARALLEL_BIAS_KEYS": {},  # No biases in RowParallelLinear layers
+            "NORM_KEYS": {
+                "input_layernorm.scale": "input_layernorm.weight",
+                "post_attention_layernorm.scale": "post_attention_layernorm.weight",
+            },
+            "FINAL_NORM_KEYS": {
+                "norm.scale": "weight",
+            },
+            "GQA_QKV_KEYS": {  # because Llama can have Grouped Query Attention and has separate Q, K, and V linear proj params, handle them separately.
+                "attention.query_key_value.weight": [
+                    "self_attn.q_proj.weight",
+                    "self_attn.k_proj.weight",
+                    "self_attn.v_proj.weight",
+                ],
+            },
         },
     },
 }
@@ -381,6 +436,16 @@ def reshard_and_split_qkv(
             state_dict[hf_key] = proj.clone()
         return state_dict
 
+def get_mlp_naming_convention(loaded_tp_ranks, layer_idx, sequential):
+    """Determine whether the checkpoint uses the legacy or new MLP naming convention."""
+    for state_dict in loaded_tp_ranks:
+        print(f'STATE_DICT: {state_dict.keys()}')
+    if any("mlp.linear1.weight" in state_dict for state_dict in loaded_tp_ranks):
+        return "new"
+    elif any("mlp.dense_h_to_4h.weight" in state_dict for state_dict in loaded_tp_ranks):
+        return "legacy"
+    else:
+        raise ValueError("Unable to determine MLP naming convention in checkpoint")
 
 def convert(
     input_checkpoint_path,
@@ -472,6 +537,16 @@ def convert(
         hf_config.vocab_size == embed_in.weight.shape[0]
     ), f"ERROR: calculated vocab size {hf_config.vocab_size} != embed param size {embed_in.shape[0]}"
     ### End Embedding Layer ###
+
+    # grab from 3rd layer to pass embeddings
+    mlp_naming = get_mlp_naming_convention(load_partitions(
+                input_checkpoint_path,
+                mp_partitions,
+                layer_idx=3,
+                sequential=sequential,
+            ), 0, sequential)
+    print(f"Detected MLP naming convention: {mlp_naming}")
+    ARCH = ARCH[mlp_naming]
 
     for layer_i in tqdm(range(get_key(loaded_config, "num-layers"))):
 
